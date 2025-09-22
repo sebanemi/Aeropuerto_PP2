8cbd46a5a7c9e37150bd51039f4e01a7de9fe6dd
sebanemi <seba.nemi16@gmail.com>
2025-09-22 11:40:22 -0300

    vs

---
+++ cola.h
@@ -0,0 +1,40 @@
+//
+// Created by seban on 22/9/2025.
+//
+#include "dominio.h"
+#ifndef AEROPUERTO_PP2_COLA_H
+#define AEROPUERTO_PP2_COLA_H
+
+#endif //AEROPUERTO_PP2_COLA_H
+
+void cargar () {
+
+
+}
+
+void prosesar () {
+
+
+}
+
+
+void deshacer () {
+
+
+}
+
+
+void mostrar () {
+
+
+}
+
+void buscar () {
+
+
+}
+
+void estadistica () {
+
+
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include AE
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
